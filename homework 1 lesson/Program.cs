ef6e1c2e7a89e4912866e951e191c0041cae526b
vsesvit <sfrol91@gmail.com>
2023-03-12 11:03:28 +0100

    first task final

--- homework 1 lesson/Program.cs
+++ homework 1 lesson/Program.cs
@@ -1,4 +1,26 @@
-﻿/*Console.WriteLine("How much is the triangle's area?");
+﻿using System;
+namespace hello_world_lesson1
+{
+    public class Program
+    {
+
+        static void Main(string[] args) /*first program*/
+        {
+            Console.WriteLine("How much is the triangle's area?");
+            Console.WriteLine("Write the base size, please");
+            var baseSize = Console.ReadLine();
+            var baseSizeInt = Convert.ToSingle(baseSize);
+            Console.WriteLine("Write the height, please");
+            var height = Console.ReadLine();
+            var heightInt = Convert.ToSingle(height);
+            float areaTriangle = (baseSizeInt + heightInt) / 2;
+
+            Console.WriteLine(areaTriangle);
+        }
+
+
+
+/*Console.WriteLine("How much is the triangle's area?");
 Console.WriteLine("Write the base size, please");
 var baseSize = Console.ReadLine();
 var baseSizeInt = Convert.ToInt32(baseSize);
