33ab4f7e756cd22ca679ad15257266471b121689
vsesvit <sfrol91@gmail.com>
2023-03-12 11:01:26 +0100

    change 1

---
+++ homework 1 lesson/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Console.WriteLine("How much is the triangle's area?");
+Console.WriteLine("Write the base size, please");
+var baseSize = Console.ReadLine();
+var baseSizeInt = Convert.ToInt32(baseSize);
+Console.WriteLine("Write the height, please");
+var height = Console.ReadLine();
+var heightInt = Convert.ToInt32(height);
+float areaTriangle = Convert.ToSingle(baseSizeInt + heightInt)/ 2;
+
+Console.WriteLine(areaTriangle);*/
+
+
+
+/*Console.WriteLine("How much is the triangle's area?");
+Console.WriteLine("Write the base size, please");
+var baseSize = Console.ReadLine();
+Console.WriteLine("Write the height, please");
+var height = Console.ReadLine();
+int areaTriangle = (Convert.ToInt32(baseSize) + Convert.ToInt32(height)) / 2;
+Console.WriteLine(areaTriangle);*/
+//floating point division
